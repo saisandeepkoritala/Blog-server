8f68dc1e60b60091400ba46f9fd4da4b96957844
saisandeepkoritala <saisandeep.koritala@gmail.com>
2024-08-18 01:01:46 -0500

    cookies

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const userRouter = require("./routes/userRoutes");
-const cors=require("cors");
-const cookieParser=require("cookie-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const session = require('express-session');
-
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+// Configure CORS
 app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:3000", "http://localhost:5173","https://saisandeep-blog.netlify.app"]
-    }));
+    origin: ["http://localhost:3000", "http://localhost:5173", "https://saisandeep-blog.netlify.app"],
+    credentials: true, // Allow cookies to be sent in cross-site requests
+}));
 
-app.use(express.static("public"))
+// Static files middleware
+app.use(express.static("public"));
 
+// Session middleware
 app.use(session({
     resave: false,
     saveUninitialized: true,
-    secret:"SECRET_HERE_BRO",
-    cookie:{
-        sameSite:'lax',
-        secure: true,
+    secret: "SECRET_HERE_BRO",
+    cookie: {
+        sameSite: 'none',  // Allow cross-site cookies
+        secure: true,      // Cookies sent over HTTPS
     }
 }));
 
-
-app.use("/api/v1/user",userRouter);
-
+// User routes
+app.use("/api/v1/user", userRouter);
 
 module.exports = app;
